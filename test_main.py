9d3b6354f223e5c70bb92ef55403090600132835
Mikkel Møller Brusen <Mikkel_mb@hotmail.com>
2018-12-01 17:55:28 +0100

    Use chunks to speed up parallel code

--- test_main.py
+++ test_main.py
@@ -45,7 +45,7 @@ def atsCorpusTest(docDict=docs,MinHashMode=None):
     print("||      Sims over threshold {:1.2f}: {:2.0f}         |                   Time: {:2.3f}                ||".format(lsh.threshold,len(sims),time.time() - start))
     print("==============================================================================================")
 
-def wikiTest(MinHashMode=None):
+def wikiTestParallel(MinHashMode=None):
     lsh = LSH(mode=MinHashMode)
     last_time = False
     j = 0
@@ -55,10 +55,10 @@ def wikiTest(MinHashMode=None):
     pool = Pool()
     while not last_time:
         filetimestart = time.time()
-        articleDict, titleDict, last_time = articleReader.findArticles("Dataset/wikitext-2/wiki.train.tokens", j)
+        articleDict, titleDict, last_time = articleReader.findArticles("Dataset/wikitext-103/wiki.train.tokens", j)
         total_file_time += time.time() - filetimestart
         iters = itertools.islice(articleDict.items(),None)
-        for key,val in tqdm(pool.imap_unordered(lsh.addDocParallel,iters),total=len(articleDict)):
+        for key,val in tqdm(pool.imap_unordered(lsh.addDocParallel,iters,chunksize=len(articleDict)//16),total=len(articleDict)):
             lsh.sigDict[key] = val
         j += 1
 
@@ -72,16 +72,53 @@ def wikiTest(MinHashMode=None):
     start = time.time()
     print("====================================== LSH similarity ========================================")
     sims = lsh.computeInternalSimilarities()
-    for sim in sims: print("|| Sim: {:05.3f} | Doc: {:31s} | Cand: {:30s} ||".format(sim[0],sim[1],sim[2]))
+    for sim in sims: print("|| Sim: {:05.3f} | Doc: {:4.0f} | Cand: {:4.0f} ||".format(sim[0],sim[1],sim[2]))
     print("========================================== Stats =============================================")
     print("||      Sims over threshold {:1.2f}: {:2.0f}         |                   Time: {:2.3f}                ||".format(lsh.threshold,len(sims),time.time() - start))
     print("==============================================================================================")
 
+def wikiTest(MinHashMode=None):
+    lsh = LSH(mode=MinHashMode)
+    last_time = False
+    j = 0
+    start = time.time()
+    total_file_time = 0
+
+
+    while not last_time:
+        filetimestart = time.time()
+        articleDict, titleDict, last_time = articleReader.findArticles("Dataset/wikitext-103/wiki.train.tokens", j)
+        total_file_time += time.time() - filetimestart
+        
+        print(articleDict[4], "JAAAAAAAAAAAA")
+        print(articleDict[5], "JAAAAAAAAAAAA")
+        print(articleDict[6], "JAAAAAAAAAAAA")
+        print(articleDict[7], "JAAAAAAAAAAAA")
+        print(articleDict[8], "JAAAAAAAAAAAA")
+        print(articleDict[9], "JAAAAAAAAAAAA")
+
+        #lsh.buildSignatures(articleDict)
+        j += 1
+
+    #lsh._buildBands()
+
+    # Printing
+    print("============================ Time spent loading files: {:3.2f}s =================================".format(total_file_time))
+    print("======================================== LSH stats ===========================================")
+    print("||   Mode: {:7s}  |   k: {:4.0f}   |   Shingle-Length: {:2.0f}   |   Bands: {:2.0f}   |   Time: {:4.0f}s  ||".format(lsh.mode,lsh.k,lsh.shinlen,lsh.b,time.time() - start))
+    print("==============================================================================================")
+    start = time.time()
+    print("====================================== LSH similarity ========================================")
+    sims = lsh.computeInternalSimilarities()
+    for sim in sims: print("|| Sim: {:05.3f} | Doc: {:4.0f} | Cand: {:4.0f} ||".format(sim[0],sim[1],sim[2]))
+    print("========================================== Stats =============================================")
+    print("||      Sims over threshold {:1.2f}: {:2.0f}         |                   Time: {:2.3f}                ||".format(lsh.threshold,len(sims),time.time() - start))
+    print("==============================================================================================")
 
 if __name__ == '__main__':
     #atsCorpusTest(MinHashMode="slow")
     #atsCorpusTest(MinHashMode="medium")
     #atsCorpusTest(MinHashMode="fast")
     #wikiTest(MinHashMode="slow")
-    wikiTest(MinHashMode="medium")
-    wikiTest(MinHashMode="fast")
+    #wikiTest(MinHashMode="medium")
+    wikiTestParallel(MinHashMode="fast")
